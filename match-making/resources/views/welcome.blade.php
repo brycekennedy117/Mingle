d40ee7cc23e291d9056d4455688d370b418991e2
campbellbrobbel <campbellbrobbel@hotmail.com>
2019-03-18 19:47:33 +1100

    Travis Build Test

--- match-making/resources/views/welcome.blade.php
+++ match-making/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Mingle
                 </div>
 
                 <div class="links">
