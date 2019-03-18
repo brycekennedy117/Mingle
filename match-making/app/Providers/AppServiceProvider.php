5b1a9a880e37961bdce687f028c251b4596bfbbb
campbellbrobbel <campbellbrobbel@hotmail.com>
2019-03-18 22:21:15 +1100

    Travis Build Test

--- match-making/app/Providers/AppServiceProvider.php
+++ match-making/app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+use function Sodium\library_version_major;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,6 +16,7 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+
     }
 
     /**
@@ -24,5 +27,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        Schema::defaultStringLength(191);
     }
 }
+
