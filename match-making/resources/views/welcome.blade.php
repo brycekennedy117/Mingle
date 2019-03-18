0c346e3f1ae05324263ce9f4d33d34733baf5fb0
campbellbrobbel <campbellbrobbel@hotmail.com>
2019-03-18 19:05:03 +1100

    Travis Build Test

--- match-making/resources/views/welcome.blade.php
+++ match-making/resources/views/welcome.blade.php
@@ -92,6 +92,7 @@
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <div>{{env('DB_HOST', '127.0.0.1')}}</div>
                 </div>
             </div>
         </div>
