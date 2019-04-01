ae1a8d844fc9e27309ddcb12c637a49ef5b9bd99
Andrew <s3559384@student.rmit.edu.au>
2019-04-01 19:20:08 +1100

    match

--- match-making/routes/web.php
+++ match-making/routes/web.php
@@ -12,9 +12,10 @@
 */
 
 Route::get('/', function () {
-    return view('login');
+    return view('matchpage');
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/matchpage', 'HomeController@index')->name('matchpage');
