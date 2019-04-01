64df39eef690b21a2cd7eba505d8eaf99ea43b5c
campbellbrobbel <campbellbrobbel@hotmail.com>
2019-04-02 10:00:58 +1100

    Attributes Route Added

--- match-making/routes/web.php
+++ match-making/routes/web.php
@@ -22,6 +22,7 @@ $router->group(['middleware' => 'auth'], function() {
     Route::get('/dashboard', 'DashboardController@index');
     Route::get('/attributes', 'AttributesController@index')->name('attributes');
     Route::get('/matches', 'MatchController@index')->name('matches');
+    Route::post('/attributes', 'AttributesController@store')->name('store_attributes');
 });
 
 $router->group(['middleware' => 'guest'], function() {
