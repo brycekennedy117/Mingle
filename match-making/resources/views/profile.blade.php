2d198d805f6951127db92b6f98e90997f84f9cff
rmit-s3603075-danny-ho <s3603075@student.rmit.edu.au>
2019-04-06 15:11:15 +1100

    profile front-end

--- match-making/resources/views/profile.blade.php
+++ match-making/resources/views/profile.blade.php
@@ -8,19 +8,46 @@
                     <div class="card-header">
                         <h1>My Profile</h1>
                     </div>
-                    <div style="padding: 20px"></div>
-                    <div class="row">
-                        <div class="col-md-6">
-                            <img src="https://via.placeholder.com/250" style="padding-left: 45px">
+                    <div class="container">
+                        <div style="padding: 20px"></div>
+                        <div class="row">
+                            <div class="col align-self-center">
+                                <img src="https://via.placeholder.com/250" class="mx-auto d-block rounded-circle">
+                            </div>
                         </div>
-                        <div class="col-md-6">
-                            <h4> Name: {{$name}}</h4>
-                            <h4> Date of birth: {{$user[0]->date_of_birth}}</h4>
-                            <h4> Postcode: {{$user[0]->postcode}}</h4>
-                            <h4> Suburb: {{$user[0]->suburb}}</h4>
-                            <h4> Gender: {{$user[0]->gender}}</h4>
-                            <h4> Interested in: {{$user[0]->interested_in}}</h4>
+                        <div style="padding: 10px"></div>
+                        <div class="row">
+                            <button type="button" class="btn btn-success mx-auto d-block">Edit Profile</button>
                         </div>
+                        <div style="padding: 20px"></div>
+                        <table class='table table-condensed table-hover text-center'>
+                            <tbody>
+                            <tr>
+                                <th>Name:</th>
+                                <td>{{$name}}</td>
+                            </tr>
+                            <tr>
+                                <th>Date of birth</th>
+                                <td>{{$user[0]->date_of_birth}}</td>
+                            </tr>
+                            <tr>
+                                <th>Postcode</th>
+                                <td>{{$user[0]->postcode}}</td>
+                            </tr>
+                            <tr>
+                                <th>Suburb</th>
+                                <td>{{$user[0]->suburb}}</td>
+                            </tr>
+                            <tr>
+                                <th>Gender</th>
+                                <td>{{$user[0]->gender}}</td>
+                            </tr>
+                            <tr>
+                                <th>Interested in</th>
+                                <td>{{$user[0]->interested_in}}</td>
+                            </tr>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
