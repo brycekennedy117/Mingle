ee47ae15be9b403ab132074f4aa89950aea4d7eb
Nathaniel <s3423170@student.rmit.edu.au>
2019-04-06 18:15:59 +1100

    Merge branch 'dev' of /Users/jarvis/Desktop/PP1-Matchmaking with conflicts.

--- match-making/resources/views/dashboard.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            Dashboard Page
-        </div>
-    </div>
-@endsection
