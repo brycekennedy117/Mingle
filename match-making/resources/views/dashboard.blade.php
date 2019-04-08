33a9123b95f804971d41da4573848b5f6aa7a516
Nathaniel <s3423170@student.rmit.edu.au>
2019-04-09 09:15:57 +1000

    added login and register

--- match-making/resources/views/dashboard.blade.php
+++ match-making/resources/views/dashboard.blade.php
@@ -1,31 +1,134 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
+
+
+    <div class="container text-center col-md-auto">
         <div class="row justify-content-center">
-            <div class="col-md-8">
+            <div class="col-md-auto">
                 <div class="card">
-                    <div class="card-header">Matched people</div>
+                    <div class="card-header font-weight-bold font">Mingles♥ near you</div>
 
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-                        <div>
-                            <div>
-                                <h2 class="" value="name"></h2>
-                                <h2 value="age"></h2>
-                            </div>
-                            <div>
-                                <img src="" alt="match"/>
-                                <info value="description">
+                    <div class="container">
+                    <div class="row justify-content-center">
 
-                                </info>
+
+                        <div class="card" style= "max-width:12rem">
+                            <img class="card-img-top" src="https://pbs.twimg.com/profile_images/784792631368945664/ZKDlomrl_400x400.jpg" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Steve rogers</h5>
+                                <p class="card-text">nal content. This content is a little bit longer.</p>
                             </div>
-                            <button class="btn btn-lg btn-primary btn-block" href="">Send message</button>
+                            <div class="card-footer">
+                                <a href="#" class="btn btn-primary">Like</a>
+                                <a href="#" class="btn btn-danger">Next</a>
+                            </div>
                         </div>
+
+                        <div class="card" style= "max-width:12rem">
+                            <img class="card-img-top" src="https://pbs.twimg.com/profile_images/712016346775564289/ajnm_P3F_400x400.jpg" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Tony Stark</h5>
+                                <p class="card-text">lead-in to.</p>
+                            </div>
+                            <div class="card-footer">
+                                <a href="#" class="btn btn-primary">Like</a>
+                                <a href="#" class="btn btn-danger">Next</a>
+                            </div>
+                        </div>
+
+                        <div class="card" style= "max-width:12rem">
+                            <img class="card-img-top" src="https://pbs.twimg.com/profile_images/848019862068666371/tYz6Y4F6_400x400.jpg" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Pepper Stark</h5>
+                                <p class="card-text">test</p>
+                            </div>
+                            <div class="card-footer">
+                                <a href="#" class="btn btn-primary">Like</a>
+                                <a href="#" class="btn btn-danger">Next</a>
+                            </div>
+                        </div>
+
+                        <div class="card" style= "max-width:12rem">
+                            <img class="card-img-top" src="https://pbs.twimg.com/profile_images/784792631368945664/ZKDlomrl_400x400.jpg" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Steve rogers</h5>
+                                <p class="card-text">nal content. This content is a little bit longer.</p>
+                            </div>
+                            <div class="card-footer">
+                                <a href="#" class="btn btn-primary">Like</a>
+                                <a href="#" class="btn btn-danger">Next</a>
+                            </div>
+                        </div>
+
+                        <div class="card" style= "max-width:12rem">
+                            <img class="card-img-top" src="https://pbs.twimg.com/profile_images/712016346775564289/ajnm_P3F_400x400.jpg" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Tony Stark</h5>
+                                <p class="card-text">lead-in to.</p>
+                            </div>
+                            <div class="card-footer">
+                                <a href="#" class="btn btn-primary">Like</a>
+                                <a href="#" class="btn btn-danger">Next</a>
+                            </div>
+                        </div>
+
+                        <div class="card" style= "max-width:12rem">
+                            <img class="card-img-top" src="https://pbs.twimg.com/profile_images/848019862068666371/tYz6Y4F6_400x400.jpg" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Pepper Stark</h5>
+                                <p class="card-text">test</p>
+                            </div>
+                            <div class="card-footer">
+                                <a href="#" class="btn btn-primary">Like</a>
+                                <a href="#" class="btn btn-danger">Next</a>
+                            </div>
+                        </div>
+
+
+
+                        <div class="card" style= "max-width:12rem">
+                            <img class="card-img-top" src="https://pbs.twimg.com/profile_images/784792631368945664/ZKDlomrl_400x400.jpg" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Steve rogers</h5>
+                                <p class="card-text">nal content. This content is a little bit longer.</p>
+                            </div>
+                            <div class="card-footer">
+                                <a href="#" class="btn btn-primary">Like</a>
+                                <a href="#" class="btn btn-danger">Next</a>
+                            </div>
+                        </div>
+
+                        <div class="card" style= "max-width:12rem">
+                            <img class="card-img-top" src="https://pbs.twimg.com/profile_images/712016346775564289/ajnm_P3F_400x400.jpg" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Tony Stark</h5>
+                                <p class="card-text">lead-in to.</p>
+                            </div>
+                            <div class="card-footer">
+                                <a href="#" class="btn btn-primary">Like</a>
+                                <a href="#" class="btn btn-danger">Next</a>
+                            </div>
+                        </div>
+
+                        <div class="card" style= "max-width:12rem">
+                            <img class="card-img-top" src="https://pbs.twimg.com/profile_images/848019862068666371/tYz6Y4F6_400x400.jpg" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Pepper Stark</h5>
+                                <p class="card-text">test</p>
+                            </div>
+                            <div class="card-footer">
+                                <a href="#" class="btn btn-primary">Like</a>
+                                <a href="#" class="btn btn-danger">Next</a>
+                            </div>
+                        </div>
+
+
+
+
+
+                    </div>
+
                     </div>
                 </div>
             </div>
