5793d51b73fa120a60b2fa9c244d8e4c341afa7d
rmit-s3603075-danny-ho <s3603075@student.rmit.edu.au>
2019-05-12 16:18:08 +1000

    modify matchmaker

--- match-making/app/MingleLibrary/MatchMaker.php
+++ match-making/app/MingleLibrary/MatchMaker.php
@@ -7,6 +7,7 @@
  */
 
 namespace App\MingleLibrary;
+use App\MingleLibrary\Models\Blocked;
 use App\MingleLibrary\Models\Ignored;
 use App\MingleLibrary\Models\Like;
 use App\MingleLibrary\Models\Match;
@@ -41,10 +42,12 @@ class MatchMaker
             $orderByRaw = "$orderByRaw$item";
         }
         $ignored = Ignored::all()->where('user_id_1', $attr->user_id)->pluck(['user_id_2'])->toArray();
+        //Blocked users
+        $blocked = Blocked::all()->where('user_id', $attr->user_id)->pluck(['blocked_id'])->toArray();
         $matchedId1 = Match::all()->where('user_id_1', $attr->user_id)->pluck('user_id_2')->toArray();
         $matchedId2 = Match::all()->where('user_id_2', $attr->user_id)->pluck(['user_id_1'])->toArray();
         $liked = Like::all()->where('user_id_1', $attr->user_id)->pluck(['user_id_2'])->toArray();
-        $ignoredUsers = array_merge($liked, $ignored, $matchedId1, $matchedId2);
+        $ignoredUsers = array_merge($liked, $ignored, $matchedId1, $matchedId2, $blocked);
         $distanceString = "round(1.60934 * 2 * 3961 * asin(sqrt(pow(sin(radians(($latitude - latitude)/2)),2) + cos(radians($latitude)) * cos(radians(latitude)) * pow(sin(radians(($longitude-longitude)/2)),2))),2)";
         $attributes =  UserAttributes::join('postcodes', 'user_attributes.postcode', '=', 'postcodes.id')
             ->where('user_id','!=', $attr['user_id'])
